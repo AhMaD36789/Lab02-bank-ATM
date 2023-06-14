c1e8140d3ed401d73dea683aa48ce999eba25f8c
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-06-14 12:13:01 +0300

    finished main functionality

--- Lab02-bank account/Program.cs
+++ Lab02-bank account/Program.cs
@@ -1,11 +1,70 @@
 ﻿namespace Lab02_bank_account
 {
-    internal class Program
+    public class Program
     {
+        public static decimal balance = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //comment to create my branch
+            try
+            {
+                UserInterface();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {Console.WriteLine("program complete");}
+        }
+        public static decimal ViewBalance()
+        {
+            return balance;
+        }
+        public static decimal Withdraw(decimal amount)
+        {
+            balance -= amount;
+            return balance;
+        }
+        public static decimal Deposit(decimal amount)
+        {
+            balance += amount;
+            return balance;
+        }
+        public static void UserInterface ()
+        {
+            
+            int userSelect = 0;
+            while (userSelect !=4) 
+            {
+                Console.WriteLine("Hello please enter the corresponding number to run any of the following operations \n 1.View Balance \t 2. Withdraw \n 3. Deposit \t \t 4. Exit");
+                userSelect = Convert.ToInt32(Console.ReadLine());
+                switch (userSelect)
+                {
+                    case 1: { 
+                            Console.WriteLine("Current balance");
+                            Console.WriteLine( ViewBalance());
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            Console.WriteLine("Select Withdraw Amount");
+                            Console.WriteLine( Withdraw(Convert.ToDecimal(Console.ReadLine())));
+
+                            break;
+                        }
+                        case 3:
+                        {
+                            Console.WriteLine("Select Deposit Amount");
+                            Console.WriteLine(Withdraw(Convert.ToDecimal(Console.ReadLine())));
+                            break;
+                        }
+                        default: {
+                            Console.WriteLine("wrong input try again");
+                            break;
+                                }
+
+                }
+            }
         }
     }
 }
