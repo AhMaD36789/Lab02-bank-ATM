708f720ed56793067b00b08df15a2a5e993c1a06
Ahmad Harhsheh <45499233+AhMaD36789@users.noreply.github.com>
2023-06-14 14:31:17 +0300

    stretch goal completed

--- Lab02-bank account/Program.cs
+++ Lab02-bank account/Program.cs
@@ -3,6 +3,8 @@
     public class Program
     {
         public static decimal balance = 10;
+        public static string[] actions = new string[10];
+        public static int actionCount = 0;
         static void Main(string[] args)
         {
             try
@@ -13,7 +15,15 @@
             {
                 Console.WriteLine(ex.Message);
             }
-            finally { Console.WriteLine("Thank you for using our services"); }
+            finally
+            {
+                Console.WriteLine("Thank you for using our services");
+                Console.WriteLine("Here is a receipt of your transactions:");
+                for (int i = 0; i < actionCount; i++)
+                {
+                    Console.WriteLine(actions[i]);
+                }
+            }
         }
         public static decimal ViewBalance()
         {
@@ -32,6 +42,8 @@
                 return -1;
             }
             balance -= amount;
+            actions[actionCount] = $"\tWithdrawal: {amount}";
+            actionCount++;
             return balance;
         }
         public static decimal Deposit(decimal amount)
@@ -42,6 +54,8 @@
                 return -1;
             }
             balance += amount;
+            actions[actionCount] = $"\tDeposit: {amount}";
+            actionCount++;
             return balance;
         }
         public static void UserInterface()
@@ -50,7 +64,7 @@
             int userSelect = 0;
             while (userSelect != 4)
             {
-                Console.WriteLine("\nPlease enter the corresponding number to run any of the following operations \n \n 1.View Balance \t 2. Withdraw \n 3. Deposit \t \t 4. Exit \n");
+                Console.WriteLine("Please enter the corresponding number to run any of the following operations \n \n 1.View Balance \t 2. Withdraw \n 3. Deposit \t \t 4. Exit \n");
                 userSelect = Convert.ToInt32(Console.ReadLine());
                 switch (userSelect)
                 {
